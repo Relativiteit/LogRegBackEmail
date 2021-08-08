a5e44f467f34f8f44687a2b12d22ac9dfac9dcc8
Relativiteit <alejocain@gmail.com>
2021-08-08 19:59:46 +0200

    Added structure files

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+/home/qlt807/IdeaProjects/LogRegBackEmailpackage com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
